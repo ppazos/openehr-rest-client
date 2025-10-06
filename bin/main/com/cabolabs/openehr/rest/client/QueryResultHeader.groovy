51483287c99288faf17aa6dd658f9c382a57f4da
Pablo Pazos <pablo.pazos@cabolabs.com>
2025-10-06 18:56:00 -0300

    removed bin

--- bin/main/com/cabolabs/openehr/rest/client/QueryResultHeader.groovy
+++
@@ -1,9 +0,0 @@
-package com.cabolabs.openehr.rest.client
-
-class QueryResultHeader {
-   
-   String archetypeId
-   String path
-   String name
-   String rmTypeName
-}
--- bin/main/com/cabolabs/openehr/rest/client/auth/Authentication.groovy
+++
@@ -1,6 +0,0 @@
-package com.cabolabs.openehr.rest.client.auth
-
-abstract class Authentication {
-
-   abstract def apply(URLConnection request)
-}
